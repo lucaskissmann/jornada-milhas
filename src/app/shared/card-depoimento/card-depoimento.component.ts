e952bde94e7eff3cf361eb9ccbfa675cf6e7a2de
Lucas Kissmann <lucaskissmann@gmail.com>
2024-08-29 11:25:05 -0300

    feat: add card-depoimento component

---
+++ src/app/shared/card-depoimento/card-depoimento.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-depoimento',
+  templateUrl: './card-depoimento.component.html',
+  styleUrls: ['./card-depoimento.component.scss']
+})
+export class CardDepoimentoComponent {
+  @Input() image: string = '';
+  @Input() text: string = '';
+  @Input() name: string = '';
+
+}
