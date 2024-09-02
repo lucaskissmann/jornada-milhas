eb101cebc9ef8c715e4607f0d30bafab018c562b
Lucas Kissmann <lucaskissmann@gmail.com>
2024-09-02 08:41:21 -0300

    refactor: updated home page to use generic component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PromocaoService } from 'src/app/core/services/promocao.service';
+import { Promocao } from 'src/app/core/types/type';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  promocoes: Promocao[] = [];
+
+  constructor(private promocaoService: PromocaoService) {}
+
+  ngOnInit(): void {
+   this.promocaoService.listar().subscribe(
+      response => {
+        this.promocoes = response;
+      },
+      error => {
+        console.error('Erro ao carregar promocoes: ', error);
+      }
+    )
+  }
 
 }
--- src/app/shared/card-busca/card-busca.component.ts
+++ src/app/shared/card-busca/card-busca.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-busca',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-busca.component.scss']
 })
 export class CardBuscaComponent {
-
+  @Input() id!: number;
+  @Input() destino!: string;
+  @Input() imagem!: string;
+  @Input() preco!: number;
 }
